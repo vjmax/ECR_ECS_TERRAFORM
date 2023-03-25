436bec4d6b4cf393619441d322209555a01712e3
vjmax <82444974+vjmax@users.noreply.github.com>
2023-03-25 18:26:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = module.exports = express()
 
 app.get('/', function(req, res){
-  res.send('hello World ');
+  res.send('Hello World ');
 });
 
 /* istanbul ignore next */
